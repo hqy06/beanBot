7d9c6ef41b5df2d5be62c1db24b072b5676f55b5
hqy06 <49214182+hqy06@users.noreply.github.com>
2019-06-17 11:35:16 -0400

    [score] H2O testie

---
+++ more_score/testie.py
@@ -0,0 +1,15 @@
+"""As always, interactive script that explores stuffs for similarity.py"""
+
+# %% import stuffs
+from importlib import reload
+import similarity
+import pandas as pd
+import numpy as np
+
+# %% testing packages
+import sys
+if 'similarity' in sys.modules:
+    print("me!")
+
+
+# %% explore data
