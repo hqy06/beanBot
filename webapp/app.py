6c7f3a7c231c405ea37f9e3ac444ff742695282a
joopica <48602646+joopica@users.noreply.github.com>
2019-06-19 10:59:09 -0400

    working messenger app, sends back responses

--- webapp/app.py
+++ webapp/app.py
@@ -5,6 +5,7 @@ from flask import request
 from flask import jsonify, make_response
 from fakeRecommender import Recommender
 
+CHATFUEL_URL = "chatfuel_api"
 FB_API_URL = 'https://graph.facebook.com/v2.6/me/messages'
 VERIFY_TOKEN = 'test_token'
 PAGE_ACCESS_TOKEN = 'EAAdrJJDrZAqIBADZAl6EEWTPR3BBoQ5sjuSPnwb1fzb4gZA2zg4zByCdlZAXvK5mh02XQarSCpVZAq3cC5YlZB4klXWxdTZB2kAmT0KqpynxZAd8ZCqrTS89s1MOzQz1ZCIfXaQeBxjDaMa3UNMVGRfS5je3yb7AHE5BRmmOfevpRvoejE9YCSjJnK'
@@ -16,7 +17,7 @@ app = Flask(__name__)
 recommender=Recommender()
 
 def get_bot_response(message):
-	return "this is a dummy response to '{}'.format(message)"
+	return "this is a dummy response to '{}'".format(message)
 
 
 def verify_webhook(req):
@@ -37,19 +38,19 @@ def is_user_message(message):
 
 def send_message(recipient_id, text):
 	""" send response to FB """
-	log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=text))
+	# log("sending message to {recipient}: {text}".format(recipient=recipient_id, text=text))
 
 	headers = {
 	"Content-Type": "application/json"
 	}
 	payload = {
-	'message': {
-		'text': text
+	"message": {
+		"text": text
 	},
-	'recipient': {
-	'id': recipient_id
+	"recipient": {
+	"id": recipient_id
 	},
-	'notification_type': 'regular'
+	"notification_type": "regular"
 	}
 
 	auth = {
@@ -59,14 +60,14 @@ def send_message(recipient_id, text):
 	response = requests.post(
 		FB_API_URL,
 		params=auth,
-		# headers=headers,
+		headers=headers,
 		json=payload
 		)
 	if response.status_code != 200:
 		log(response.status_code)
 		log(response.text)
 
-	return response.json()
+	return response
 
 
 @app.route("/webhook", methods=['GET','POST'])
@@ -77,25 +78,26 @@ def listen():
 
 	if request.method == 'POST':
 		payload = request.json
-		event = payload['entry'][0]['messaging']
-		for x in event:
-			if is_user_message(x):
-				text = x['message']['text']
-				sender_id = x['sender']['id']
-				respond(sender_id, text)
+		# event = payload['entry'][0]['messaging']
+		# for x in event:
+		# 	if is_user_message(x):
+		# 		text = x['message']['text']
+		# 		sender_id = x['sender']['id']
+		# 		respond(sender_id, text)
 
 		return "Hello World", 200
 
 
 @app.route("/testing", methods=['GET','POST'])
 def json():
-	# url = "chatfuel_api"
-	# data = json.load(urllib2.urlopen(url))
+	""" returns a message to messenger if json file successfully received """
 	if request.json:
 		mydata = request.json
+		text = "thanks i got it!"
 		return make_response(jsonify({
 			"messages": [
-			{"text": "Thanks, 200"}
+			{"text": text
+			}
 			]
 			}))
 	else:
@@ -105,24 +107,13 @@ def json():
 			]
 			}
 
-
-# def testing():
-# 	if request.method == 'GET':
-# 		data=request.get_json(force=True)
-# 		return make_response(jsonify(data))
-
-# 	if request.method == 'POST':
-# 		payload = request.get_json(force=True)
-# 		return make_response(jsonify(payload))
-
-
 # This route will receive and parse some JSON data and then return a JSON response using the recommender
 @app.route("/rec", methods=['GET', 'POST'])
 def chatbot():
 	recommendation = "default: conselling"
-	if request.method == 'POST':
+	if request.json:
 		# a Python dictionary with JSON fields serielized into key/value pairs
-		data=request.get_json(force=True)
+		data=request.json
 		# feed data into the recommender to get get recommendation
 		recommendation = recommender.recommend(data)
 		# return a JSON response
